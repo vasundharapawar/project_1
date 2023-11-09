91fe4c8f49fdf49d01509375e54c7af7601f94ae
vasundharapawar <pawarvasundhara199@gmail.com>
2023-11-09 19:07:51 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def vasundhara(request):
+    return render(request,'vasundhara.html')
+def maya(request):
+    return render(request,'maya.html')
+def rithu(request):
+    return render(request,'rithu.html')
+def rashi(request):
+    return render(request,'rashi.html')
+
+
